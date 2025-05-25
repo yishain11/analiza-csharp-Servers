c965fc4807d0b9101cfa24fc3ace75fd87c19e2b
yishain11 <yishain11@walla.co.il>
2025-05-25 07:51:04 +0300

    added http req

--- requests/Requests.cs
+++ requests/Requests.cs
@@ -1,14 +1,35 @@
 ﻿using System.Net.Http;
 using System;
 using System.Threading.Tasks;
+using System.Text;
 
 public static class MyHttpReq
 {
+    public static HttpClient client;
+
     public static async Task SendHttpGET()
     {
         HttpClient client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync(" https://api.thecatapi.com/v1/images/search");
             string content = await response.Content.ReadAsStringAsync();
             Console.WriteLine(content);
+    }
+
+    public static async Task SendHttpGETWithHeaders()
+    {
+        HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "CSharpApp");
+        HttpResponseMessage response = await client.GetAsync(" https://api.thecatapi.com/v1/images/search");
+        string content = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(content);
+    }
+
+    public static async Task SendHttpPOSTithHeaders()
+    {
+        HttpClient client = new HttpClient();
+        var data = new StringContent("{\"key\":\"value\"}", Encoding.UTF8, "application/json");
+        var postResponse = await client.PostAsync("https://httpbin.org/post", data);
+        string content = await postResponse.Content.ReadAsStringAsync();
+        Console.WriteLine(content);
     }
 }
