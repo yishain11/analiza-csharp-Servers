3b2981f3eac6be56f16fc751ff9b58ac62970ea3
yishain11 <yishain11@walla.co.il>
2025-05-25 07:00:40 +0300

    added http req

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,49 @@ namespace Servers
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task runServer()
         {
+            string url = "http://localhost:8080/";
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+
+            Console.WriteLine($"Listening at {url}...");
+
+            while (true)
+            {
+                // Wait for an incoming request
+                HttpListenerContext context = await listener.GetContextAsync();
+
+                // Get request and response objects
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+
+                Console.WriteLine($"[{DateTime.Now}] {request.HttpMethod} {request.Url}");
+
+                // Build response
+                string responseString = "<html><body><h1>Hello from C# HTTP Server!</h1></body></html>";
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+                response.ContentLength64 = buffer.Length;
+                response.ContentType = "text/html";
+
+                // Write the response
+                using (var output = response.OutputStream)
+                {
+                    await output.WriteAsync(buffer, 0, buffer.Length);
+                }
+            }
+
+        }
+
+
+
+        public static async Task Main(string[] args)
+        {
+            //await runServer();
+            Task t = MyHttpReq.SendHttpGET();
+            await t;
         }
     }
 }
---
+++ requests/Requests.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System;
+using System.Threading.Tasks;
+
+public static class MyHttpReq
+{
+    public static async Task SendHttpGET()
+    {
+        HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(" https://api.thecatapi.com/v1/images/search");
+            string content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+    }
+}
